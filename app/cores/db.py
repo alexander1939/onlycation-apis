58f4351eb7208f18810bfd622d638f505f6327ca
Alex Cifuentes <alexcifuentes72818@gamil.com>
2025-11-09 14:29:24 -0600

    feat: Conexion a la base de datos del servidor

--- app/cores/db.py
+++ app/cores/db.py
@@ -1,32 +1,15 @@
 """
-Configuración de SQLAlchemy para trabajar con una base de datos SQLite de forma asincrónica.
+Configuración de SQLAlchemy para trabajar con una base de datos MySQL de forma asincrónica.
 """
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 
-
-""" 
-Crear el motor de la base de datos asincrónico.
-    - Usa SQLite como base de datos local.
-    - Se desactiva 'check_same_thread' porque SQLite no es thread-safe por defecto.
-    - 'echo=False' evita mostrar las consultas SQL en consola.
-"""
 engine = create_async_engine(
-    "sqlite+aiosqlite:///task.db",
-    connect_args={"check_same_thread": False},  
+    "mysql+aiomysql://onlycation:OnlyCation2025@database-onlycationdb-1rcvau:3306/onlycation_db",
     echo=False
 )
 
-
-
-""" 
-Crear un generador de sesiones asincrónicas para interactuar con la base de datos.
-    - Usa AsyncSession como clase base para la sesión.
-    - autocommit=False: se debe confirmar manualmente cada transacción.
-    - autoflush=False: evita que los cambios se envíen automáticamente antes de consultar.
-    - expire_on_commit=False: los objetos no se invalidan después del commit.
-"""
 async_session = async_sessionmaker(
     engine,
     class_=AsyncSession,
